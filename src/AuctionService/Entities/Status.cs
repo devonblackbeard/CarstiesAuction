359a46d86b420acae2f91908a5d09982c4996f8b
devonblackbeard <devon.blackbeard@gmail.com>
2023-11-09 13:15:59 -0700

    Create CRUD endpoints

---
+++ src/AuctionService/Entities/Status.cs
@@ -0,0 +1,5 @@
+namespace AuctionService.Entities;
+
+public enum Status {
+    Live, Finished, ReserveNotMet
+}
---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,33 @@
+using AuctionService;
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt => {
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")); // takes connection string
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+app.Run();
