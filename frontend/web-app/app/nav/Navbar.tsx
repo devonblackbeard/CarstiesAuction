a83e610d868feba481fed9ed50851db7b43ab625
devonblackbeard <devon.blackbeard@gmail.com>
2024-01-29 13:04:48 -0700

    Create basic frontend layout

---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div>Left</div>
+        <div>Middle</div>
+        <div>Right</div>
+    </header>
+
+    )
+}
+
+export default Navbar
---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./nav/Navbar"
+
+export default function Home() {
+  return (
+    <div>
+      <h3 className="text-3xl font-semibold">Welcome to Carsties!</h3>
+    </div>
+  );
+}
