fb2a8f05276eb5f3f75f4ee6526d662ea92623c6
devonblackbeard <devon.blackbeard@gmail.com>
2024-02-07 19:18:11 -0700

    Fix sorting on search and tweak configuration for identity service to work with Authjs

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -5,37 +5,37 @@ namespace SearchService;
 
 [ApiController]
 [Route("api/search")]
-public class SearchController: ControllerBase
+public class SearchController : ControllerBase
 {
     [HttpGet]
     public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
         var query = DB.PagedSearch<Item, Item>();
-        
+
         if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
             query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
 
-        query = searchParams.FilterBy switch 
+        query = searchParams.FilterBy switch
         {
             "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
             "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
             _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
         };
 
-        query = searchParams.OrderBy switch 
+        query = searchParams.OrderBy switch
         {
-            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "make" => query.Sort(x => x.Ascending(a => a.Make)).Sort(x => x.Ascending(a => a.Model)),
             "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
             _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
         };
 
-        if(!string.IsNullOrEmpty(searchParams.Seller))
+        if (!string.IsNullOrEmpty(searchParams.Seller))
         {
             query.Match(x => x.Seller == searchParams.Seller);
         }
-        if(!string.IsNullOrEmpty(searchParams.Winner))
+        if (!string.IsNullOrEmpty(searchParams.Winner))
         {
             query.Match(x => x.Seller == searchParams.Winner);
         }
