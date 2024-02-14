d62ef9caa2572e066bee788bd30426278ca16268
devonblackbeard <devon.blackbeard@gmail.com>
2024-02-13 22:32:53 -0700

    Implement Auction create form using react datepicker

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,8 +1,14 @@
+import Heading from '@/app/components/Heading'
 import React from 'react'
+import AuctionForm from '../AuctionForm'
 
 const Create = () => {
   return (
-    <div>Create Auction</div>
+    <div className='mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'>
+      <Heading title="Sell your car!" subtitle='Please enter the details of your car' />
+
+      <AuctionForm />
+    </div>
   )
 }
 
