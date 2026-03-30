ba6ffef6c619d266f2aa85373369d7e4d53d8072
Devon <devon.blackbeard@gmail.com>
2026-03-30 05:01:38 -0600

    Run a production build

--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import DuendeIdentityServer6 from "next-auth/providers/duende-identity-server6";
+import NextAuth from "next-auth";
+import { authOptions } from "./options";
 
-export const authOptions: NextAuthOptions = {
-    session: {
-        strategy: 'jwt'
-    },
-    providers: [
-        DuendeIdentityServer6({
-            id: 'id-server',
-            clientId: 'nextApp',
-            clientSecret: 'secret',
-            issuer: 'http://localhost:5000',
-            authorization: { params: { scope: 'openid profile auctionApp' } },
-            idToken: true
-        })
-    ],
-    callbacks: {
-        async jwt({token, profile, account}) {
-            if(profile) token.username = profile.username
-            if (account && account.access_token) token.accessToken = account.access_token
-            return token            
-        },
-        async session({session, token}) {
-            if(token){
-                session.user.username = token.username
-                session.accessToken = token.accessToken
-            }
-            return session
-        }
-    }
-
-}
-
-
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
